3aa685172e83b741920b3e5e1ba65aa95021c200
Zach_Fellows <65567611+angelsfan1522@users.noreply.github.com>
2022-07-13 11:28:47 -0700

    Initial commit
    Create Calculator.r

---
+++ Calculator.r
@@ -0,0 +1,17 @@
+#Building a R Simple Calculator 
+
+print("choice of operations")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+
+choice <- as.integer(readline(prompt = "Enter Your Choice: "))
+
+num1 <- as.double(readline(prompt = "Enter Your First Number: "))
+num2 <- as.double(readline(prompt = "Enter Your Second Number: "))
+
+result <-switch(choice,(num1+num2),(num1-num2),(num1*num2),(num1/num2))
+
+print(paste("The Result is: ", result))
