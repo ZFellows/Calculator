98f176e295b32971985bc02d3fa4e79ceb05d368
Zach_Fellows <65567611+angelsfan1522@users.noreply.github.com>
2022-07-13 17:12:54 -0700

    Powers added

--- Calculator.r
+++ Calculator.r
@@ -5,6 +5,7 @@ print("1. Addition")
 print("2. Subtraction")
 print("3. Multiplication")
 print("4. Division")
+print("5. Power")
 
 
 choice <- as.integer(readline(prompt = "Enter Your Choice: "))
@@ -12,6 +13,6 @@ choice <- as.integer(readline(prompt = "Enter Your Choice: "))
 num1 <- as.double(readline(prompt = "Enter Your First Number: "))
 num2 <- as.double(readline(prompt = "Enter Your Second Number: "))
 
-result <-switch(choice,(num1+num2),(num1-num2),(num1*num2),(num1/num2))
+result <-switch(choice,(num1+num2),(num1-num2),(num1*num2),(num1/num2),(num1 ^ num2))
 
 print(paste("The Result is: ", result))
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Calculator
  Simple Calculator
+
+
+this runs line by line in R studio but flawless in Visual Code Studio it runs through the  prompts.
+ I created a simple calculator which you can use at you lesisuire feel free to  make this more complex as you wish this is a simple calculator. 
